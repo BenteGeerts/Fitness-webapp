4bd85b1718f6a5b891bdadaac5c835c74dfb5053
BenteG <bente.geerts@student.kdg.be>
2023-04-25 16:11:01 +0200

    added gulp and fixed css/js build + added bootstrap

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+const gulp = require('gulp');
+const { parallel, series } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sassGlob = require('gulp-sass-glob');
+const watch = require('watch');
+const browserSync = require('browser-sync').create();
+const imagemin = require('gulp-imagemin');
+const cssnano = require('gulp-cssnano');
+const minifyJs = require('gulp-uglify');
+const rename = require("gulp-rename");
+var concat = require('gulp-concat');
+
+
+function buildStyle(){
+    return gulp.src('./resources/scss/app.scss')
+    .pipe(sassGlob())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cssnano())
+        .pipe(rename("style.css"))
+    .pipe(gulp.dest('./public/css'));
+}
+function imageMin(){
+    return gulp.src('./resources/images/*.{jpg,png,svg}')
+    .pipe(imagemin([
+        imagemin.mozjpeg({quality:80, progressive: true}),
+        imagemin.optipng({optimizationLevel: 2})
+        ]))
+    .pipe(gulp.dest('./public/images'));
+}
+function js(){
+    return gulp.src('./resources/**/*.js')
+    .pipe(minifyJs())
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('./public/js'));
+}
+function watchAll() {
+    gulp.watch('./public/index.php', browsersyncReload);
+    gulp.watch('./resources/**/*.scss', series(buildStyle, browsersyncReload));
+    gulp.watch('./resources/**/*.js', series(js, browsersyncReload));
+    gulp.watch('./resources/**/*.{jpg,png,svg}', series(imageMin, browsersyncReload));
+}
+function browsersyncServe(cb){
+    browserSync.init({
+        server: {
+            baseDir: './public'
+        }
+    });
+    cb();
+}
+function browsersyncReload(cb){
+    browserSync.reload();
+    cb();
+}
+exports.dev = series(browsersyncServe, watchAll);
+exports.build = parallel(buildStyle, js, imageMin);
