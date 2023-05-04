e8606b7f4dfb47cbce298d5cf857d1342c923b3f
BenteG <bente.geerts@student.kdg.be>
2023-05-04 17:01:46 +0200

    added styling to landing page and updated html

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -1,7 +1,30 @@
 @extends('layouts.app')
 @section('content')
+    <section class="landingpage">
+        <div class="row">
+            <div class="col-md-12 col-xl-6">
+                <div class="landingpage__hero">
+                    <img class="landingpage__hero-img" src="/images/hero.png" alt="women wearing gym clothes">
+                </div>
+            </div>
 
-    <a href="{{Route("googleLogin")}}">Login with Google</a>
-    <a href="{{Route("facebookLogin")}}">Login with Facebook</a>
+            <div class="col-md-12 col-xl-6">
+                <div class="landingpage__text-content">
+                    <div class="landingpage__heading">
+                        <h1 class="landingpage__heading-title">MOVE</h1>
+                        <p class="landingpage__heading-slogan">Motivate Overcome Visualize Empower</p>
+                    </div>
 
+                    <a class="button button--big" href="">Sign Up with Email</a>
+
+                    <div class="landingpage__buttons">
+                        <a class="button button--secondary button--icon" href="{{Route("googleLogin")}}">Google</a>
+                        <a class="button button--secondary button--icon" href="{{Route("facebookLogin")}}">Facebook</a>
+                    </div>
+
+                    <p>Already have an account? Sign in</p>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
