e236df9b4d4b3fdb620335afe570bc8452d4f00e
BenteG <bente.geerts@student.kdg.be>
2023-05-04 17:00:41 +0200

    created nav

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,7 @@
+<nav class="nav">
+    <i class="icon-home nav__item"></i>
+    <i class="icon-Friends nav__item"></i>
+    <i class="icon-training nav__item"></i>
+    <i class="icon-profile nav__item"></i>
+    <i class="icon-shop nav__item"></i>
+</nav>
