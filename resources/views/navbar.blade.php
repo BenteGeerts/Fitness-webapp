2a4afcdd0ca536bcb8aa4b96ff054cb7303aa1a7
BenteG <bente.geerts@student.kdg.be>
2023-05-08 15:13:18 +0200

    updated icon name in navbar

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="nav">
     <i class="icon-home nav__item"></i>
-    <i class="icon-Friends nav__item"></i>
+    <i class="icon-friends nav__item"></i>
     <i class="icon-training nav__item"></i>
     <i class="icon-profile nav__item"></i>
     <i class="icon-shop nav__item"></i>
