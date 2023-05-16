386201de4163332df50e823ced3a7b2e8e01201d
BenteG <bente.geerts@student.kdg.be>
2023-05-16 10:26:14 +0200

    added linkst to navbar and removed text-decoration

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="nav">
-    <i class="icon-home nav__item"></i>
-    <i class="icon-friends nav__item"></i>
-    <i class="icon-training nav__item"></i>
-    <i class="icon-profile nav__item"></i>
-    <i class="icon-shop nav__item"></i>
+    <a href="{{Route("home")}}" class="nav__link"><i class="icon-home nav__item"></i></a>
+    <a href="" class="nav__link"><i class="icon-friends nav__item"></i></a>
+    <a href="" class="nav__link"><i class="icon-training nav__item"></i></a>
+    <a href="" class="nav__link"><i class="icon-profile nav__item"></i></a>
+    <a href="" class="nav__link"><i class="icon-shop nav__item"></i></a>
 </nav>
