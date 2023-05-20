7bd86119b481581a1b562b9072863d605a1c38ff
BenteG <bente.geerts@student.kdg.be>
2023-05-20 17:45:40 +0200

    added profilevariant page

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
@@ -9,5 +10,11 @@ class ProfileController extends Controller
     public function profile()
     {
         return view("profile");
+    }
+
+    public function show($id)
+    {
+        $user = User::find($id);
+        return view("profile-variant")->with("user", $user);
     }
 }
