bfe013b25907267d737e8834ae12c382d34067c1
BenteG <bente.geerts@student.kdg.be>
2023-05-20 10:41:17 +0200

    updated navbar with active class and check for active

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="nav">
-    <a href="{{Route("home")}}" class="nav__link"><i class="icon-home nav__item"></i></a>
-    <a href="" class="nav__link"><i class="icon-friends nav__item"></i></a>
+    <a href="{{ route("home") }}" class="nav__link{{ request()->routeIs('home') ? ' nav__link--active' : '' }}"><i class="icon-home nav__item"></i></a>
+    <a href="{{route("friends")}}" class="nav__link{{ request()->routeIs('friends') ? ' nav__link--active' : '' }}"><i class="icon-friends nav__item"></i></a>
     <a href="" class="nav__link"><i class="icon-training nav__item"></i></a>
     <a href="" class="nav__link"><i class="icon-profile nav__item"></i></a>
-    <a href="" class="nav__link"><i class="icon-shop nav__item"></i></a>
+    <a href="{{Route("shop")}}" class="nav__link"><i class="icon-shop nav__item"></i></a>
 </nav>
