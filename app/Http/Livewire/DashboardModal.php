e5d5f91850e20355681b1824f51e312f3047b9e7
BenteG <bente.geerts@student.kdg.be>
2023-05-21 12:09:37 +0200

    added error showing functions to livewire component and blade file

--- app/Http/Livewire/DashboardModal.php
+++ app/Http/Livewire/DashboardModal.php
@@ -2,12 +2,34 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\User;
 use Livewire\Component;
 
 class DashboardModal extends Component
 {
+    public $showModal;
+    public $searchTerm;
+    public $showError = false;
+    public $showAvailable = false;
+
     public function render()
     {
         return view('livewire.dashboard-modal');
+    }
+
+    public function changeUsername()
+    {
+        $username = User::where('username', '=', $this->searchTerm)->first();
+
+        if(!is_null($username))
+        {
+            $this->showAvailable = false;
+            $this->showError = true;
+        }
+        if(is_null($username))
+        {
+            $this->showError = false;
+            $this->showAvailable = true;
+        }
     }
 }
--- resources/views/livewire/dashboard-modal.blade.php
+++ resources/views/livewire/dashboard-modal.blade.php
@@ -1,13 +1,22 @@
 <div class="dashboard__modal">
-   <div class="dashboard__modal-text">
-       <span>Currently your username is: </span>
-       <span>You can update your username here.</span>
-   </div>
+    <div class="dashboard__modal-text">
+        <span>Currently your username is: <b>{{auth()->user()->username}}</b></span>
+        <span>You can update your username here.</span>
+    </div>
 
     <div class="form__field">
         <label class="form__label" for="username">Username</label>
-        <input class="form__input-field" name="username" id="username" type="email">
+        <input wire:model="searchTerm" wire:change="changeUsername" class="form__input-field" name="username" id="username" type="email"
+               placeholder="Preferred username">
+        @if($showError)
+            <span> Oops! Username already taken!</span>
+        @endif
+
+        @if($showAvailable)
+            <span>Alright! Username available!</span>
+        @endif
     </div>
 
     <button class="button">Save</button>
 </div>
+
