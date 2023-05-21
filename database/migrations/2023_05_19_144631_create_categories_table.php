a3c5948a0db7a720db533f1d67773c62458ad523
BenteG <bente.geerts@student.kdg.be>
2023-05-21 15:08:59 +0200

    fixed issue with migration because of migration order

--- database/migrations/2023_05_19_144631_create_categories_table.php
+++ database/migrations/2023_05_19_144631_create_categories_table.php
@@ -13,13 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('exercises', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string("name");
-            $table->string("image_url");
-            $table->string("video_url");
-            $table->string("common_mistakes");
-            $table->integer("diamonds");
+            $table->string("category");
             $table->timestamps();
         });
     }
@@ -31,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('categories');
     }
 };
