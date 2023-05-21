0eaebc8e95f9f4fb88891614355c96c6039eaed7
BenteG <bente.geerts@student.kdg.be>
2023-05-21 14:13:17 +0200

    added logic to update username

--- app/Http/Livewire/DashboardModal.php
+++ app/Http/Livewire/DashboardModal.php
@@ -3,14 +3,21 @@
 namespace App\Http\Livewire;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class DashboardModal extends Component
 {
-    public $showModal;
+    public bool $showModal = false;
     public $searchTerm;
     public $showError = false;
     public $showAvailable = false;
+    public $user;
+
+    public function mount($showModal)
+    {
+        $this->showModal = $showModal;
+    }
 
     public function render()
     {
@@ -19,17 +26,36 @@ class DashboardModal extends Component
 
     public function changeUsername()
     {
-        $username = User::where('username', '=', $this->searchTerm)->first();
+        $this->validate([
+            'searchTerm' => 'required',
+        ]);
 
-        if(!is_null($username))
+        $this->user = User::where('username', '=', $this->searchTerm)->first();
+
+        if(!is_null($this->user))
         {
             $this->showAvailable = false;
             $this->showError = true;
         }
-        if(is_null($username))
+        if(is_null($this->user))
         {
             $this->showError = false;
             $this->showAvailable = true;
+        }
+    }
+
+    public function save()
+    {
+        $this->validate([
+            'searchTerm' => 'required',
+        ]);
+
+        if(is_null($this->user))
+        {
+            $user = Auth::user();
+            $user->username = $this->searchTerm;
+            $user->update();
+            $this->showModal = false;
         }
     }
 }
