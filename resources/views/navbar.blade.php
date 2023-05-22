83fa31ffb6f996304aed1fdd48ce8f7acdba0817
BenteG <bente.geerts@student.kdg.be>
2023-05-22 13:25:13 +0200

    added last link to navigationbar

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -2,7 +2,7 @@
     <nav class="nav">
         <a href="{{ route('home') }}" class="nav__link{{ request()->routeIs('home') ? ' nav__link--active' : '' }}"><i class="icon-home nav__item"></i></a>
         <a href="{{route('friends')}}" class="nav__link{{ request()->routeIs('friends') ? ' nav__link--active' : '' }}"><i class="icon-friends nav__item"></i></a>
-        <a href="" class="nav__link"><i class="icon-training nav__item"></i></a>
+        <a href="{{route('training')}}" class="nav__link{{ request()->routeIs('training') ? ' nav__link--active' : '' }}"><i class="icon-training nav__item"></i></a>
         <a href="{{route('profile')}}" class="nav__link{{ request()->routeIs('profile') ? ' nav__link--active' : '' }}"><i class="icon-profile nav__item"></i></a>
         <a href="{{route('shop')}}" class="nav__link{{ request()->routeIs('shop') ? ' nav__link--active' : '' }}"><i class="icon-shop nav__item"></i></a>
     </nav>
