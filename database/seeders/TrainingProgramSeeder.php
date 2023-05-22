60ed8636b6881ce1476600d5df0a9ca1e98ea772
BenteG <bente.geerts@student.kdg.be>
2023-05-22 15:58:01 +0200

    created first basic training program

--- database/seeders/TrainingProgramSeeder.php
+++ database/seeders/TrainingProgramSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class TrainingProgramSeeder extends Seeder
 {
@@ -14,6 +15,34 @@ class TrainingProgramSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table("training_program_has_exercise")->insert([
+            "training_program_id" => 1,
+            "exercise_id" => 16
+        ]);
+
+        DB::table("training_program_has_exercise")->insert([
+            "training_program_id" => 1,
+            "exercise_id" => 18
+        ]);
+
+        DB::table("training_program_has_exercise")->insert([
+            "training_program_id" => 1,
+            "exercise_id" => 22
+        ]);
+
+        DB::table("training_program_has_exercise")->insert([
+            "training_program_id" => 1,
+            "exercise_id" => 20
+        ]);
+
+        DB::table("training_program_has_exercise")->insert([
+            "training_program_id" => 1,
+            "exercise_id" => 8
+        ]);
+
+        DB::table("training_program_has_exercise")->insert([
+            "training_program_id" => 1,
+            "exercise_id" => 19
+        ]);
     }
 }
