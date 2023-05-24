e6cf0a52702d3c788fe87c0f3b004389c3ab88dd
BenteG <bente.geerts@student.kdg.be>
2023-05-24 17:08:01 +0200

    catched the session data in the TrainingController

--- app/Http/Controllers/TrainingController.php
+++ app/Http/Controllers/TrainingController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Exercise;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class TrainingController extends Controller
 {
@@ -13,7 +15,18 @@ class TrainingController extends Controller
 
     public function newTraining()
     {
-        return view("new-training");
+        $exercises = session('selectedExercises');
+
+        if(!empty($exercises))
+        {
+            $exercises = Exercise::whereIn("name", $exercises)->get();
+        }
+        $diamonds = 0;
+        foreach($exercises as $exercise)
+        {
+            $diamonds += $exercise->diamonds;
+        }
+        return view("new-training", ["exercises" => $exercises, "diamonds" => $diamonds]);
     }
 
     public function addExercise()
