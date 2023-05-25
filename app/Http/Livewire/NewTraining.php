9f2ba4a411d4a9341c61345dd97a6c5970e79592
BenteG <bente.geerts@student.kdg.be>
2023-05-25 11:06:29 +0200

    added logic to save custom created traningprogram

--- app/Http/Livewire/NewTraining.php
+++ app/Http/Livewire/NewTraining.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use App\Models\Exercise;
+use App\Models\TrainingProgram;
 use Illuminate\Database\Eloquent\Collection;
 use Livewire\Component;
 
@@ -10,24 +11,58 @@ class NewTraining extends Component
 {
     public $diamonds;
     public $exercises;
+    public $decodedString;
+
+    public $trainingName;
 
 
     public function mount($exercises)
     {
-        $decodedString = html_entity_decode($exercises);
-        $this->exercises = collect(json_decode($decodedString))->map(function ($item) {
-            return (object) $item;
-        });
+        $this->decodedString = html_entity_decode($exercises);
+        $this->exercises = $this->toObject();
     }
 
     public function render()
     {
+        $this->exercises = collect(json_decode($this->decodedString))->map(function ($item) {
+            return (object) $item;
+        });
         return view('livewire.new-training');
+    }
+
+    public function save()
+    {
+        $this->validate([
+            'trainingName' => 'required',
+        ]);
+
+        $training = new TrainingProgram();
+        $training->name = $this->trainingName;
+        $training->level_id = 4;
+        $training->total_diamonds = $this->diamonds;
+        $training->user_id = auth()->id();
+        $training->save();
+
+        $this->exercises = $this->toObject();
+        $idArray = $this->exercises->pluck('id')->toArray();
+
+        $training->exercises()->attach($idArray);
+
+
+        session()->forget('selectedExercises');
+        return redirect()->route("training");
     }
 
     public function cancel()
     {
         session()->forget('selectedExercises');
         return redirect()->route('training');
+    }
+
+    public function toObject()
+    {
+        return collect(json_decode($this->decodedString))->map(function ($item) {
+            return (object) $item;
+        });
     }
 }
