705facc8e99578d2f3cd31df4766587e0aeaf813
BenteG <bente.geerts@student.kdg.be>
2023-05-30 14:11:30 +0200

    added livewire "eventlistener" to add class to body

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,3 +4,16 @@ import "./flatpickr";
 import "./friends";
 import "./register";
 import "./setup";
+
+Livewire.on('addBodyClass', function (classNames) {
+    if (Array.isArray(classNames)) {
+        classNames.forEach(function (className) {
+            document.body.classList.toggle(className);
+        });
+    } else {
+        var classList = classNames.split(' ');
+        classList.forEach(function (className) {
+            document.body.classList.toggle(className);
+        });
+    }
+});
