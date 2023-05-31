6607f6e4218eb0eeaa14050a33136fad0f53f66e
BenteG <bente.geerts@student.kdg.be>
2023-05-31 15:44:45 +0200

    added trait with streak check functionallity

---
+++ app/Traits/StreakTrait.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Traits;
+
+use App\Models\Achievement;
+use App\Models\TrainingProgramsHistory;
+use App\Models\User;
+use Carbon\Carbon;
+
+trait StreakTrait
+{
+    public static function checkStreak($userId)
+    {
+        $currentDate = now()->toDateString();
+
+        $achievement = Achievement::where('user_id', $userId)
+            ->whereDate('created_at', $currentDate)
+            ->first();
+
+        if (!$achievement) {
+            Achievement::create([
+                'user_id' => $userId,
+                'date' => $currentDate,
+            ]);
+        }
+
+        $trainingsCount = TrainingProgramsHistory::where('user_id', $userId)
+            ->whereDate('created_at', $currentDate)
+            ->count();
+
+        if ($trainingsCount <= 1) {
+            Achievement::where('id', $userId)
+                ->increment('streak_length');
+        }
+    }
+}
