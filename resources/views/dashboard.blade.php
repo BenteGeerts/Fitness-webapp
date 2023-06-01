b6c8af4d56231b6a7f1e4f7034550d09ee9de2d5
BenteG <bente.geerts@student.kdg.be>
2023-06-01 10:41:19 +0200

    added links to training detail on training cards on the dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -152,7 +152,8 @@
                                     </div>
 
                                     <div class="program__text-container">
-                                        <p class="program__title">{{$program->name}}</p>
+                                        <a href="{{route('training.detail', ['slug' => $program->slug])}}"><p
+                                                class="program__title">{{$program->name}}</p></a>
                                         <div class="program__icons">
                                             <div class="program__icon-wrapper">
                                                 <i class="icon-alarm program__icon"></i>
