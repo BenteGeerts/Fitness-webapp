4b8d81b5ff7abdb2021d2701e51439debd0fea23
BenteG <bente.geerts@student.kdg.be>
2023-06-01 20:57:52 +0200

    added logic to get the last lifted weight on your last training

--- app/Traits/TrainingTrait.php
+++ app/Traits/TrainingTrait.php
@@ -3,9 +3,10 @@
 namespace App\Traits;
 
 use App\Models\Exercise;
+use App\Models\ExerciseHistory;
 use App\Models\UserData;
 
-Trait TrainingTrait
+trait TrainingTrait
 {
     public static function calculateDiamonds($exerciseId, $reps, $weight)
     {
@@ -15,6 +16,28 @@ Trait TrainingTrait
         $userData = UserData::where('user_id', auth()->id())->first();
 
 
-        return round(($baseDiamonds * $userData->age * $reps * $weight * ($userData->height)/ 100) / 1000);
+        return round(($baseDiamonds * $userData->age * $reps * $weight * ($userData->height) / 100) / 1000);
+    }
+
+    public static function getLastTrainingWeight()
+    {
+
+        $history = ExerciseHistory::where('user_id', auth()->id())->latest()->first();
+
+        if (isset($history)) {
+            $timestamp = $history->created_at;
+
+            $weights = ExerciseHistory::where('user_id', auth()->id())->where('created_at', $timestamp)->get();
+
+            $totalWeight = 0;
+            foreach ($weights as $weight) {
+                $totalWeight += $weight->weight * $weight->reps;
+            }
+
+            return $totalWeight;
+        }
+        if (!isset($history)) {
+            return 0;
+        }
     }
 }
