640f95f858e7c160086169376c77f90f52c91a7d
BenteG <bente.geerts@student.kdg.be>
2023-06-01 11:12:46 +0200

    created trainingTrait and added function to calculate diamonds

---
+++ app/Traits/TrainingTrait.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Traits;
+
+use App\Models\Exercise;
+use App\Models\UserData;
+
+Trait TrainingTrait
+{
+    public static function calculateDiamonds($exerciseId, $reps, $weight)
+    {
+        $exercise = Exercise::find($exerciseId)->first();
+        $baseDiamonds = $exercise->diamonds;
+
+        $userData = UserData::where('user_id', auth()->id())->first();
+
+
+        return ($baseDiamonds * $userData->age * $reps * $weight * ($userData->height)/ 100) / 1000;
+    }
+}
