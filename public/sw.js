658232d6e6f90aff9d130be8ef3d9f51ece86e2b
BenteG <bente.geerts@student.kdg.be>
2023-06-02 19:31:09 +0200

    cached first assets

--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,28 @@
+const staticCacheName = 'site-static';
+const assets = [
+    '/',
+    '/css/style.css',
+    '/login',
+];
+
 //install service worker
 self.addEventListener('install', evt => {
-    console.log('service worker has been installed');
-})
+        //console.log('service worker has been installed');
+        evt.waitUntil(
+            caches.open(staticCacheName).then(cache => {
+                console.log('caching assets');
+                cache.addAll(assets);
+            })
+        );
+    }
+);
 
 //active event
 self.addEventListener('activate', evt => {
-    console.log('service worker has been activated');
+    //console.log('service worker has been activated');
 })
 
 //fetch event
 self.addEventListener('fetch', evt => {
-    console.log('fetch event', evt);
+    //console.log('fetch event', evt);
 })
