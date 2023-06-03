21179f89498b81316aa1e98cf48523f6f50c1910
BenteG <bente.geerts@student.kdg.be>
2023-06-03 16:21:40 +0200

    added vite scss resource to app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,14 +19,13 @@
     <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
 
     <!-- Scripts -->
-    @vite(['resources/js/app.js'])
+    @vite(['resources/js/app.js', 'resources/scss/app.scss'])
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet">
     <!-- Styles -->
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     @livewireStyles
 
 </head>
