217ddd248aef318c31ee631d9516a2853fdc714f
BenteG <bente.geerts@student.kdg.be>
2023-06-05 13:29:08 +0200

    added back button to training detail page

--- resources/views/training-detail.blade.php
+++ resources/views/training-detail.blade.php
@@ -4,6 +4,7 @@
         <div class="row justify-content-center">
             <div class="col-md-12 col-lg-9 col-xl-10">
                 <div class="page__heading">
+                    <a class="no-underline" href="{{route('training')}}"><i class="icon-back page__heading-icon"></i></a>
                     <h1>Training</h1>
                 </div>
                 <livewire:training-detail :training='$training'/>
