539e144ba5faf2bab6daf8a7e6851d366a8648a0
BenteG <bente.geerts@student.kdg.be>
2023-06-05 15:19:18 +0200

    added livewire component to profile page

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -4,29 +4,17 @@
         <div class="row justify-content-center">
             <div class="col-md-12 col-lg-10">
                 <div class="page__heading page__heading--space">
-                   <div class="page__heading">
-                       <a class="no-underline" href="{{route('home')}}"><i class="icon-back page__heading-icon"></i></a>
-                       <h1>Profile</h1>
-                   </div>
-                    <a class="page__heading-settings" href="{{route('settings')}}"><i class="icon-settings page__settings"></i></a>
+                    <div class="page__heading">
+                        <a class="no-underline" href="{{route('home')}}"><i
+                                class="icon-back page__heading-icon"></i></a>
+                        <h1>Profile</h1>
+                    </div>
+                    <a class="page__heading-settings" href="{{route('settings')}}"><i
+                            class="icon-settings page__settings"></i></a>
                 </div>
 
-                <div class="profile__wrapper">
-                    <div class="profile__img-wrapper">
-                        <img class="profile__img" src="{{$user->avatar}}" alt="">
-
-                        <label class="profile__img-label" for="profileimg">
-                            <i class="icon-camera profile__img-edit"></i>
-                        </label>
-
-                        <input class="profile__img-input" type="file" id="profileimg" accept="image/png, image/jpeg">
-                    </div>
-
-                    <div class="profile__name-wrapper">
-                        <p class="profile__name">{{$user->name}}</p>
-                        <span class="profile__friends">{{count($user->friends)}} friends</span>
-                    </div>
-                </div>
+                <livewire:profile-image avatar="{{$user->avatar}}" name="{{$user->name}}"
+                                        friends="{{count($user->friends)}}"/>
 
                 <h2 class="h3">Statistics</h2>
                 <div class="profile__statistics">
