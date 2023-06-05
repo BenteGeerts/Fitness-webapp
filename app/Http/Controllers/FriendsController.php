047df3ee63051111c361bf94c397f100c898e62a
BenteG <bente.geerts@student.kdg.be>
2023-06-05 13:25:40 +0200

    added logic in friendscontroller to fill the friends and world leaderboard

--- app/Http/Controllers/FriendsController.php
+++ app/Http/Controllers/FriendsController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Achievement;
+use App\Models\Friend;
 use Illuminate\Http\Request;
 
 class FriendsController extends Controller
@@ -9,7 +11,17 @@ class FriendsController extends Controller
     public function friends()
     {
         $user = auth()->user();
-        $friends = $user->friends;
-        return view("friends")->with("friends", $friends);
+        $friends = $user->friends()->with('achievement')->get()->sortByDesc(function ($friend) {
+            return $friend->achievement->total_diamonds;
+        });
+
+
+
+        $worldLeaderboard = Achievement::with('user')
+            ->take(10)
+            ->orderBy('total_diamonds', 'desc')
+            ->get();
+
+        return view("friends", ['friends' => $friends, 'worldLeaderboard' => $worldLeaderboard]);
     }
 }
