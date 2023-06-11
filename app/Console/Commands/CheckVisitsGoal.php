f03784560b9eba3f8d11fe69e08e1929212100c8
BenteG <bente.geerts@student.kdg.be>
2023-06-11 11:31:04 +0200

    updated cronjob logic

--- app/Console/Commands/CheckVisitsGoal.php
+++ app/Console/Commands/CheckVisitsGoal.php
@@ -14,7 +14,7 @@ class CheckVisitsGoal extends Command
      *
      * @var string
      */
-    protected $signature = 'check:visits-goal';
+    protected $signature = 'visits-goal:check';
 
     /**
      * The console command description.
