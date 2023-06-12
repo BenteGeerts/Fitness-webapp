ef639d7d7b46dbca1373ebfdfad025c435f2d4c4
BenteG <bente.geerts@student.kdg.be>
2023-06-12 20:50:25 +0200

    wrote logic for setup edit

--- app/Http/Controllers/SetupController.php
+++ app/Http/Controllers/SetupController.php
@@ -8,7 +8,28 @@ use Illuminate\Support\Facades\Auth;
 
 class SetupController extends Controller
 {
-    public function setup() {
+    public function setup()
+    {
         return view("setup.setup");
+    }
+
+    public function edit()
+    {
+        $userData = auth()->user()->userData;
+        $weight = $userData->weight;
+        $height = $userData->height;
+        $age = $userData->age;
+        $gender = $userData->gender->gender;
+        $goal = $userData->goal->goal;
+        $minVisits = $userData->min_visits;
+        return view("setup.setup-edit",
+            [
+            'weight' => $weight,
+            'height' => $height,
+            'age' => $age,
+            'gender' => $gender,
+            'goal' => $goal,
+            'minVisits' => $minVisits
+        ]);
     }
 }
