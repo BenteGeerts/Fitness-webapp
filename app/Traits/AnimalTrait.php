4a6ef413a2f20271f08d7f9d84db6eacaa73f0b7
BenteG <bente.geerts@student.kdg.be>
2023-06-12 10:33:19 +0200

    created animal trait and wrote animal comparison logic

---
+++ app/Traits/AnimalTrait.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Traits;
+
+use App\Models\Animal;
+
+trait AnimalTrait
+{
+    public static function animalCompareWeight($lastLiftedWeight)
+    {
+        $animals = Animal::all();
+        $animals = $animals->sortByDesc('weight');
+
+        $comparisonResult = "";
+
+        $borderWeight = 1000;
+        if ($lastLiftedWeight < $borderWeight) {
+            foreach ($animals as $animal) {
+                if ($lastLiftedWeight >= $animal->weight) {
+
+                    $animalCount = floor($lastLiftedWeight / $animal->weight);
+                    $comparisonResult .= $animalCount . " " . $animal->name . ", ";
+                    $lastLiftedWeight %= $animal->weight;
+                }
+            }
+
+            $comparisonResult = rtrim($comparisonResult, ", ");
+        } else {
+            $largestAnimal = $animals->first();
+            $comparisonResult = "1 " . $largestAnimal->name;
+        }
+
+        return $comparisonResult;
+    }
+}
