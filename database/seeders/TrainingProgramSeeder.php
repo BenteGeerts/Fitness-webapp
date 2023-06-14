f8f297b1480c028300af0aedaa9c24b60411fdae
BenteG <bente.geerts@student.kdg.be>
2023-06-14 11:39:48 +0200

    filled in other trainingprograms

--- database/seeders/TrainingProgramSeeder.php
+++ database/seeders/TrainingProgramSeeder.php
@@ -15,6 +15,7 @@ class TrainingProgramSeeder extends Seeder
      */
     public function run()
     {
+        //push
         DB::table("training_program_has_exercise")->insert([
             "training_program_id" => 1,
             "exercise_id" => 16
@@ -43,6 +44,161 @@ class TrainingProgramSeeder extends Seeder
         DB::table("training_program_has_exercise")->insert([
             "training_program_id" => 1,
             "exercise_id" => 19
+        ]);
+
+
+        //pull
+        DB::table("training_program_has_exercise")->insert([
+            "training_program_id" => 2,
+            "exercise_id" => 12
+        ]);
+
+        DB::table("training_program_has_exercise")->insert([
+            "training_program_id" => 2,
+            "exercise_id" => 14
+        ]);
+
+        DB::table("training_program_has_exercise")->insert([
+            "training_program_id" => 2,
+            "exercise_id" => 13
+        ]);
+
+        DB::table("training_program_has_exercise")->insert([
+            "training_program_id" => 2,
+            "exercise_id" => 23
+        ]);
+
+        DB::table("training_program_has_exercise")->insert([
+            "training_program_id" => 2,
+            "exercise_id" => 15
+        ]);
+
+        DB::table("training_program_has_exercise")->insert([
+            "training_program_id" => 2,
+            "exercise_id" => 10
+        ]);
+
+
+        //legs
+        DB::table("training_program_has_exercise")->insert([
+            "training_program_id" => 3,
+            "exercise_id" => 28
+        ]);
+
+        DB::table("training_program_has_exercise")->insert([
+            "training_program_id" => 3,
+            "exercise_id" => 30
+        ]);
+
+        DB::table("training_program_has_exercise")->insert([
+            "training_program_id" => 3,
+            "exercise_id" => 31
+        ]);
+
+        DB::table("training_program_has_exercise")->insert([
+            "training_program_id" => 3,
+            "exercise_id" => 32
+        ]);
+
+        DB::table("training_program_has_exercise")->insert([
+            "training_program_id" => 3,
+            "exercise_id" => 34
+        ]);
+
+        DB::table("training_program_has_exercise")->insert([
+            "training_program_id" => 3,
+            "exercise_id" => 35
+        ]);
+
+
+        //full body
+        DB::table("training_program_has_exercise")->insert([
+            "training_program_id" => 4,
+            "exercise_id" => 16
+        ]);
+
+        DB::table("training_program_has_exercise")->insert([
+            "training_program_id" => 4,
+            "exercise_id" => 12
+        ]);
+
+        DB::table("training_program_has_exercise")->insert([
+            "training_program_id" => 4,
+            "exercise_id" => 21
+        ]);
+
+        DB::table("training_program_has_exercise")->insert([
+            "training_program_id" => 4,
+            "exercise_id" => 22
+        ]);
+
+        DB::table("training_program_has_exercise")->insert([
+            "training_program_id" => 4,
+            "exercise_id" => 30
+        ]);
+
+
+        //upper body
+        DB::table("training_program_has_exercise")->insert([
+            "training_program_id" => 5,
+            "exercise_id" => 16
+        ]);
+
+        DB::table("training_program_has_exercise")->insert([
+            "training_program_id" => 5,
+            "exercise_id" => 13
+        ]);
+
+        DB::table("training_program_has_exercise")->insert([
+            "training_program_id" => 5,
+            "exercise_id" => 14
+        ]);
+
+        DB::table("training_program_has_exercise")->insert([
+            "training_program_id" => 5,
+            "exercise_id" => 22
+        ]);
+
+        DB::table("training_program_has_exercise")->insert([
+            "training_program_id" => 5,
+            "exercise_id" => 27
+        ]);
+
+        DB::table("training_program_has_exercise")->insert([
+            "training_program_id" => 5,
+            "exercise_id" => 20
+        ]);
+
+
+        //lower body
+        DB::table("training_program_has_exercise")->insert([
+            "training_program_id" => 6,
+            "exercise_id" => 36
+        ]);
+
+        DB::table("training_program_has_exercise")->insert([
+            "training_program_id" => 6,
+            "exercise_id" => 34
+        ]);
+
+        DB::table("training_program_has_exercise")->insert([
+            "training_program_id" => 6,
+            "exercise_id" => 35
+        ]);
+
+        DB::table("training_program_has_exercise")->insert([
+            "training_program_id" => 6,
+            "exercise_id" => 33
+        ]);
+
+        DB::table("training_program_has_exercise")->insert([
+            "training_program_id" => 6,
+            "exercise_id" => 31
+        ]);
+
+        DB::table("training_program_has_exercise")->insert([
+            "training_program_id" => 6,
+            "exercise_id" => 32
         ]);
     }
 }
