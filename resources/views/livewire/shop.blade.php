68a4626f0c82b2e20c70a1dda43b414aedf89a1d
BenteG <bente.geerts@student.kdg.be>
2023-06-14 13:22:54 +0200

    created livewire component for shop

---
+++ resources/views/livewire/shop.blade.php
@@ -0,0 +1,40 @@
+<div class="row">
+    <div class="col-md-12 col-lg-6">
+        <div class="shop__item">
+            <div class="shop__item-wrapper">
+                <i class="icon-fire shop__item-icon shop__item-icon--orange"></i>
+            </div>
+
+            <div class="shop__item-text">
+                <p class="shop__item-title">Streak freeze</p>
+                <p class="shop__item-paragraph">Streak Freeze allows your streak to
+                    remain in place for one full day of inactivity.
+                </p>
+
+                <a class="shop__price-wrapper">
+                    <i class="icon-diamond shop__price-icon"></i>
+                    <span class="shop__price">9999</span>
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-12 col-lg-6">
+        <div class="shop__item shop__item--bottom">
+            <div class="shop__item-wrapper">
+                <i class="icon-diamond shop__item-icon shop__item-icon--red"></i>
+            </div>
+
+            <div class="shop__item-text">
+                <p class="shop__item-title">Double points</p>
+                <p class="shop__item-paragraph">Double points allows you to get
+                    double points for the next 24 hours.
+                </p>
+
+                <a class="shop__price-wrapper">
+                    <i class="icon-diamond shop__price-icon"></i>
+                    <span class="shop__price">9999</span>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
