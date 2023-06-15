56ae62051a6c188d791d6b60c2bd984d1464460b
BenteG <bente.geerts@student.kdg.be>
2023-06-15 19:36:00 +0200

    added extra filter so that new users have one week to reach their gym visits

--- app/Console/Commands/CheckVisitsGoal.php
+++ app/Console/Commands/CheckVisitsGoal.php
@@ -6,6 +6,7 @@ use App\Models\Achievement;
 use App\Models\TrainingProgramsHistory;
 use App\Models\User;
 use Illuminate\Console\Command;
+use Carbon\Carbon;
 
 class CheckVisitsGoal extends Command
 {
@@ -30,22 +31,25 @@ class CheckVisitsGoal extends Command
      */
     public function handle()
     {
-        $users = User::all();
+        $weekAgo = Carbon::now()->subWeek()->toDateString();
+        $users = User::whereDate('created_at', '<', $weekAgo)->get();
 
-        foreach ($users as $user) {
+        if(isset($users) && count($users) > 0) {
+            foreach ($users as $user) {
 
-            $minimumVisitsGoal = $user->userData->min_visits;
-
-
-            $visitsCount = TrainingProgramsHistory::where('user_id', $user->id)
-                ->whereBetween('created_at', [now()->subWeek(), now()])
-                ->count();
+                $minimumVisitsGoal = $user->userData->min_visits;
 
 
-            if (!$visitsCount >= $minimumVisitsGoal) {
-                $achievement = Achievement::where('user_id', $user->id)->first();
-                $achievement->streak_length = 0;
-                $achievement->update();
+                $visitsCount = TrainingProgramsHistory::where('user_id', $user->id)
+                    ->whereBetween('created_at', [now()->subWeek(), now()])
+                    ->count();
+
+
+                if (!$visitsCount >= $minimumVisitsGoal) {
+                    $achievement = Achievement::where('user_id', $user->id)->first();
+                    $achievement->streak_length = 0;
+                    $achievement->update();
+                }
             }
         }
 
