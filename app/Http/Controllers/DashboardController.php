6c3d3cbae4fcf119d2539ebbc4c1b5a5dd0166f0
BenteG <bente.geerts@student.kdg.be>
2023-06-18 12:32:22 +0200

    code cleanup: dashboardController

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,13 +7,12 @@ use App\Models\User;
 use App\Models\UserData;
 use App\Traits\AnimalTrait;
 use App\Traits\DiamondTrait;
+use App\Traits\FitTrait;
 use App\Traits\StreakTrait;
 use App\Traits\TrainingTrait;
 use Illuminate\Http\Request;
 use Google\Client;
 use Google\Service\Fitness;
-use GuzzleHttp\Client as GuzzleClient;
-use GuzzleHttp\Psr7\Request as GuzzleRequest;
 use Illuminate\Support\Facades\Auth;
 use mysql_xdevapi\Exception;
 
@@ -22,92 +21,17 @@ class DashboardController extends Controller
     use StreakTrait;
     use DiamondTrait;
     use TrainingTrait;
+    use FitTrait;
 
     public function Fit()
     {
-        if (session('showChart')) {
-            $client = new Client();
-            $client->setApplicationName(env('GOOGLE_APP_NAME'));
-            $client->setClientId(env('GOOGLE_CLIENT_ID'));
-            $client->setClientSecret(env('GOOGLE_CLIENT_SECRET'));
-            $client->setRedirectUri(env('GOOGLE_REDIRECT_URI'));
-            $client->setAccessType('offline');
-            $client->setIncludeGrantedScopes(true);
 
-            $accessToken = session()->get('access_token');
-            $refreshToken = auth()->user()->refresh_token;
-
-
-            if (isset($refreshToken)) {
-                if ($client->isAccessTokenExpired()) {
-
-                    $client->fetchAccessTokenWithRefreshToken($refreshToken);
-                    $newAccessToken = $client->getAccessToken();
-
-                    session()->put('access_token', $newAccessToken['access_token']);
-
-                    $client->setAccessToken($newAccessToken['access_token']);
-
-                    $accessToken = session()->get('access_token');
-                }
-
-                // Set up the request parameters
-                $url = 'https://www.googleapis.com/fitness/v1/users/me/dataset:aggregate';
-                $data_source_id = 'derived:com.google.step_count.delta:com.google.android.gms:estimated_steps';
-                $end_time = strtotime('tomorrow');
-                $start_time = strtotime('-1 week', $end_time);
-
-                $data_request = array(
-                    'aggregateBy' => array(
-                        array('dataTypeName' => 'com.google.step_count.delta')
-                    ),
-                    'bucketByTime' => array(
-                        'durationMillis' => 86400000
-                    ),
-                    'startTimeMillis' => $start_time * 1000,
-                    'endTimeMillis' => $end_time * 1000,
-                    'dataSourceId' => $data_source_id,
-                );
-
-                $headers = [
-                    'Content-Type' => 'application/json',
-                    'Authorization' => 'Bearer ' . $accessToken,
-                ];
-
-                $request = new GuzzleRequest('POST', $url, [
-                    'Authorization' => 'Bearer ' . $accessToken,
-                    'Content-Type' => 'application/json',
-                ], json_encode($data_request));
-                $client = new GuzzleClient();
-                $response = $client->send($request);
-
-                $data = json_decode($response->getBody()->getContents(), true);
-
-                foreach ($data['bucket'] as $bucket) {
-                    $startTimeMillis = $bucket['startTimeMillis'];
-                    $endTimeMillis = $bucket['endTimeMillis'];
-                    if (!isset($bucket['dataset'][0]['point'][0]['value'][0]['intVal'])) {
-                        continue;
-                    }
-                    $stepCount = $bucket['dataset'][0]['point'][0]['value'][0]['intVal'];
-                    $dateTime = date('d-m', $startTimeMillis / 1000);
-                    $stepCountData[] = [
-                        'date' => $dateTime,
-                        'stepCount' => $stepCount,
-                    ];
-                }
-
-                if (isset($stepCountData)) {
-                    session(['stepCountData' => $stepCountData]);
-                }
-            }
-        }
-
+        FitTrait::makeRequest();
 
         $user = User::find(auth()->id());
         $friends = $user->friends;
 
-        $userData = UserData::where('user_id', Auth::id())->first();
+        $userData = $user->userData;
         $recommendedPrograms = TrainingProgram::where("level_id", "=", $userData->level_id)->orWhere([
             ['level_id', 4],
             ['user_id', auth()->id()]])->with('level')->get();
@@ -115,7 +39,6 @@ class DashboardController extends Controller
         $streakLength = StreakTrait::retreiveLength(auth()->id());
         $diamonds = DiamondTrait::getDiamonds();
         $lastLiftedWeight = TrainingTrait::getLastTrainingWeight();
-
         $animalComparison = AnimalTrait::animalCompareWeight($lastLiftedWeight);
 
 
