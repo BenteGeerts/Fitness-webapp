6ff977cfbce6c8d71175abe70332e757ba45a046
BenteGeerts <92160442+BenteGeerts@users.noreply.github.com>
2026-04-09 11:46:15 +0200

    updated exercise detail page ux/ui

--- resources/views/exercise.blade.php
+++ resources/views/exercise.blade.php
@@ -11,40 +11,34 @@
                     <h1>Exercise</h1>
                 </div>
 
-                <div class="exercise__heading">
-                    <h2 class="exercise__name">
-                        {{ $exercise->name }}
-                    </h2>
-                    <p class="exercise__description">
-                        <i class="icon-diamond program__icon--purple"></i>
-                        {{ $exercise->diamonds }} diamonds per set
-                    </p>
+                <div class="exercise__hero">
+                    <img class="exercise__hero-img" src="{{ $exercise->image_url }}"
+                         alt="{{ $exercise->name }} demonstration">
+                    <div class="exercise__hero-overlay"></div>
+                    <div class="exercise__hero-content">
+                        <h2 class="exercise__name">{{ $exercise->name }}</h2>
+                        <span class="exercise__badge">
+                            <i class="icon-diamond exercise__badge-icon"></i>
+                            {{ $exercise->diamonds }} diamonds per set
+                        </span>
+                    </div>
                 </div>
 
-                <div class="row">
-                    <div class="col-md-12 col-lg-6">
-                        <div class="exercise__img-wrapper">
-                            <img class="exercise__img" src="{{ $exercise->image_url }}"
-                                 alt="{{ $exercise->name }} demonstration">
-                        </div>
-
-                        <div class="exercise__section">
-                            <h3 class="h3 exercise__title">Common mistakes</h3>
-                            <ol class="exercise__mistakes-list">
-                                @foreach (explode("\n", $exercise->common_mistakes) as $mistake)
-                                    @if(trim($mistake))
-                                        <li>{{ str_replace('\n', '', e($mistake)) }}</li>
-                                    @endif
-                                @endforeach
-                            </ol>
-                        </div>
+                <div class="exercise__grid">
+                    <div class="exercise__card">
+                        <h3 class="exercise__card-title">Common mistakes</h3>
+                        <ol class="exercise__mistakes-list">
+                            @foreach (explode("\n", $exercise->common_mistakes) as $mistake)
+                                @if(trim($mistake))
+                                    <li class="exercise__mistakes-item">{{ str_replace('\n', '', e($mistake)) }}</li>
+                                @endif
+                            @endforeach
+                        </ol>
                     </div>
 
-                    <div class="col-md-12 col-lg-6">
-                        <h3 class="h3 exercise__title exercise__title--top">
-                            Tutorial video
-                        </h3>
-                        <div class="embed-container">
+                    <div class="exercise__card">
+                        <h3 class="exercise__card-title">Tutorial video</h3>
+                        <div class="exercise__video">
                             <iframe src="{{ $exercise->video_url }}" frameborder="0" allowfullscreen></iframe>
                         </div>
                     </div>
